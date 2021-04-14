73d0b5637b25a8cf17f25babbc6d8d36010ad3f9
Shahriar Hossain <9763691+Shahriarhossain123@users.noreply.github.com>
2021-04-14 23:45:20 +0600

    take2

--- Number Guessing Game/View/PlayerVsAppVC.swift
+++ Number Guessing Game/View/PlayerVsAppVC.swift
@@ -8,14 +8,43 @@
 import UIKit
 
 class PlayerVsAppVC: UIViewController {
-
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var helpButton: UIButton!
+    @IBOutlet weak var gressButton: UIButton!
+    @IBOutlet weak var playerVappTitleLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var moveLabel: UILabel!
+    @IBOutlet weak var hintLabel: UILabel!
+    @IBOutlet weak var setnumberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        pVaViewOutlet()
         // Do any additional setup after loading the view.
     }
     
-
+    func pVaViewOutlet() {
+        //Button title label
+        backButton.titleLabel?.text = Strings.back_button
+        
+        
+        //Button title label color
+        backButton.titleLabel?.textColor = .white
+        
+        //info text
+        playerVappTitleLabel.text = Strings.player_vs_app
+        playerVappTitleLabel.textColor = Colors.violetColor
+    }
+    
+    @IBAction func backButtonAction(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func gressButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func helpButtonAction(_ sender: UIButton) {
+    }
     /*
     // MARK: - Navigation
 
