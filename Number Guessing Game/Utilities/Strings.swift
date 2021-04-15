ec901278498046ede08aa9718708ac0691ac175a
Shahriar Hossain <9763691+Shahriarhossain123@users.noreply.github.com>
2021-04-15 17:59:43 +0600

    take3

--- Number Guessing Game/Utilities/Strings.swift
+++ Number Guessing Game/Utilities/Strings.swift
@@ -17,6 +17,8 @@ struct Strings{
     static let set_button = "Set"
     static let gress_button = "Gress"
     static let cancel_button = "Cancel"
+    static let okey_button = "Okey"
+    static let restart_button = "Restart"
     
     static let player_vs_player = "Player Vs Player"
     static let player_vs_app = "Player Vs App"
@@ -26,16 +28,26 @@ struct Strings{
     static let player_vs_player_title_get = "Gress the number that first player given."
     
     static let more_move = "more move"
+    static let lets_guess = "Lets Guess the number"
     static let quit_alert_massage = "Do you went to quit this app"
     static let too_low_number = "The number is too low."
     static let low_number = "The number is low."
-    static let too_high_number = "The number is too hight."
-    static let high_number = "The number is hight."
+    static let too_high_number = "The number is too high."
+    static let high_number = "The number is high."
+    
+    static let congratulation = "Congratulations"
+    static let get_the_number = "You get the number."
+    static let won = "You WON the game"
+    static let lose = "You LOSE the game"
+    static let bad_guess = "Bad Guess"
     
     static let player_vs_app_help = "The app is going to randomly select an integer from 1 to 100. You'll keep guessing numbers until you find the app's number, and the app will tell you each time if your guess was too high or too low. but the catch is that you only have 10 tries to guess the correct number!"
     
     static let player_vs_player_help = "One player is going to randomly select an integer from 1 to 100. You'll keep guessing numbers until you find the player's number, and the app will tell you each time if your guess was too high or too low. but the catch is that you only have 10 tries to guess the correct number!"
     
     static let info = "The app or player is going to randomly select an integer from 1 to 100. You’ll keep guessing numbers until you find the app’s or player’s number and the app will tell you each time if your guess was too high or too low. but the the catch is that you only have 10 tries to guess the correct number!"
+    
+    static let text_is_empty = "You didn't enter the number!"
+    static let numbers = "! NUMBER !"
     
 }
--- Number Guessing Game/View/PlayerVsAppVC.swift
+++ Number Guessing Game/View/PlayerVsAppVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 class PlayerVsAppVC: UIViewController {
     @IBOutlet weak var backButton: UIButton!
@@ -17,42 +18,141 @@ class PlayerVsAppVC: UIViewController {
     @IBOutlet weak var hintLabel: UILabel!
     @IBOutlet weak var setnumberTextField: UITextField!
     
+    var moves = 10
+    var appanswer = Int.random(in: 1...100)
+    var playeranswer = 0
+    var win = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         pVaViewOutlet()
         // Do any additional setup after loading the view.
     }
     
+    // MARK: - Function
     func pVaViewOutlet() {
         //Button title label
         backButton.titleLabel?.text = Strings.back_button
         
-        
         //Button title label color
         backButton.titleLabel?.textColor = .white
+        gressButton.titleLabel?.textColor = .white
+        playerVappTitleLabel.textColor = Colors.violetColor
+        infoLabel.textColor = Colors.violetColor
+        moveLabel.textColor = Colors.violetColor
+        hintLabel.textColor = Colors.violetColor
+        setnumberTextField.textColor = Colors.violetColor
         
         //info text
         playerVappTitleLabel.text = Strings.player_vs_app
-        playerVappTitleLabel.textColor = Colors.violetColor
+        infoLabel.text = Strings.player_vs_app_title
+        moveLabel.text = "\(moves) " + Strings.more_move
+        hintLabel.text = "HINT:\n" + Strings.lets_guess
     }
+    
+    func mathAnswer(getplayer: Int, setApp: Int) -> Bool {
+        var conWin = false
+        if (moves > 0){
+            var takeEx = 0
+            moves -= 1
+            if (getplayer < setApp) {
+                takeEx = setApp - getplayer
+                if (takeEx < 10) {
+                    moveLabel.text = "\(moves) " + Strings.more_move
+                    hintLabel.text = "HINT:\n" + Strings.low_number
+                } else {
+                    moveLabel.text = "\(moves) " + Strings.more_move
+                    hintLabel.text = "HINT:\n" + Strings.too_low_number
+                }
+            } else if (getplayer > setApp){
+                takeEx = getplayer - setApp
+                if (takeEx < 10) {
+                    moveLabel.text = "\(moves) " + Strings.more_move
+                    hintLabel.text = "HINT:\n" + Strings.high_number
+                } else {
+                    moveLabel.text = "\(moves) " + Strings.more_move
+                    hintLabel.text = "HINT:\n" + Strings.too_high_number
+                }
+            } else if (getplayer == setApp){
+                moveLabel.text = "\(moves) " + Strings.more_move
+                hintLabel.text = "HINT:\n" + Strings.congratulation + ". " + Strings.get_the_number
+                conWin = true
+            }
+        } else{
+            conWin = false
+        }
+        return conWin
+    }
+    
+    func emptyText(){
+        // create the alert
+        let alert = UIAlertController(title: Strings.numbers, message: Strings.text_is_empty, preferredStyle: UIAlertController.Style.alert)
+        // add the actions (buttons)
+        let cancel = UIAlertAction(title: Strings.okey_button, style: UIAlertAction.Style.cancel, handler: nil)
+        alert.addAction(cancel)
+        // show the alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func winAlert(res: Bool){
+        if res == true {
+            // create the alert
+            let alert = UIAlertController(title: Strings.congratulation, message: Strings.won, preferredStyle: UIAlertController.Style.alert)
+            // add the actions (buttons)
+            let cancel = UIAlertAction(title: Strings.okey_button, style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(cancel)
+            let quit = UIAlertAction(title: Strings.restart_button, style: UIAlertAction.Style.destructive, handler: { [self] action in
+                pVaViewOutlet()
+            })
+            alert.addAction(quit)
+            
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            // create the alert
+            let alert = UIAlertController(title: Strings.bad_guess, message: Strings.lose, preferredStyle: UIAlertController.Style.alert)
+            // add the actions (buttons)
+            let cancel = UIAlertAction(title: Strings.okey_button, style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(cancel)
+            let quit = UIAlertAction(title: Strings.restart_button, style: UIAlertAction.Style.destructive, handler: { [self] action in
+                pVaViewOutlet()
+            })
+            alert.addAction(quit)
+            
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    // MARK: - Button
     
     @IBAction func backButtonAction(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func gressButtonAction(_ sender: UIButton) {
+        if (setnumberTextField.text != "") {
+            playeranswer = Int(setnumberTextField.text!)!
+            win = mathAnswer(getplayer: playeranswer, setApp: appanswer)
+        } else{
+            emptyText()
+        }
+        
     }
     
     @IBAction func helpButtonAction(_ sender: UIButton) {
     }
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
